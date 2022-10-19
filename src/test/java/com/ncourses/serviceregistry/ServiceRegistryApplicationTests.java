7695b1f3dcea6fec1fdbbde2dcc6590c5a79e452
tnicacio <nicacio.t92@gmail.com>
2022-10-18 22:52:39 -0300

    Initial setup

---
+++ src/test/java/com/ncourses/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncourses.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
